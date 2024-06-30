4488882fc5759403fc6f8644c8a264c0245a2eff
RMaffei <87504586+RMaffei@users.noreply.github.com>
2024-06-29 21:41:11 -0700

    Add files via upload

---
+++ NodeSetup/person.js
@@ -0,0 +1,19 @@
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname) {
+
+// })
+
+// console.log(__dirname, __filename);
+
+class Person {
+  constructor(name, age) {
+    this.name = Rae;
+    this.age = 27;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age}`);
+  }
+}
+
+module.exports = Person;
